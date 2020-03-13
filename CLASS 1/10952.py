cd965188fc6caf9635f0b3f152fbd1fe0cf669f5
leeanna96 <ablean820@gmail.com>
2020-03-13 18:07:05 +0900

    20200313 CLASS 1

--- CLASS 1/10952.py
+++ CLASS 1/10952.py
@@ -1,7 +1,8 @@
-n=int(input())
 num=[]
-for i in range(n):
+while True:
     a,b=map(int,input().split())
+    if a==0 and b==0:
+        break
     num.append(a+b)
-for i in range(n):
+for i in range(len(num)):
     print(num[i])
---
+++ CLASS 1/2562.py
@@ -0,0 +1,7 @@
+num=[]
+for i in range(9):
+    a=int(input())
+    num.append(a)
+a=max(num)
+print(a)
+print(num.index(a)+1)
